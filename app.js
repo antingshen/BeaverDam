8301d55e647f0863211538db99fd0cbb1757acde
Allen Wang <allen.y.w@berkeley.edu>
2016-05-30 09:41:03 -0700

    Code cleanup

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-
-video_name = "test_vid"
-total_frames = 99
-
-function frame_url(frame) {
-    block_0 = Math.floor(frame / 10000)
-    block_1 = Math.floor(frame / 100)
-    return `videos/${video_name}/${block_0}/${block_1}/${frame}.jpg`
-}
-
-
+document.addEventListener("DOMContentLoaded", function(event) {
+  new Canvas(document.getElementById('frame'));
+});
--- shapes.js
+++ shapes.js
@@ -1,348 +1,90 @@
-function Shape(x, y, w, h, fill) {
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 1;
-  this.h = h || 1;
-  this.fill = fill || '#AAAAAA';
-}
-function within_top (shape, mx, my) {
-  return (shape.y + 4 >= my) && (shape.y - 4 <= my)
-}
-
-function within_bottom (shape, mx, my) {
-  return (shape.y + shape.h + 4 >= my) && (shape.y + shape.h - 4 <= my)
-}
-
-function within_right (shape, mx, my) {
-  return (shape.x + shape.w + 4 >= mx) && (shape.x + shape.w - 4 <= mx);
-}
-
-function within_left(shape, mx, my) {
-  return (shape.x + 4 >= mx) && (shape.x - 4 <= mx);
-}
-Shape.prototype.draw = function(ctx) {
-  ctx.fillStyle = this.fill;
-  ctx.fillRect(this.x, this.y, this.w, this.h);
-}
-
-Shape.prototype.contains = function(mx, my) {
-  var minx = Math.min(this.x, this.x + this.w);
-  var maxx = Math.max(this.x, this.x + this.w);
-  var miny = Math.min(this.y, this.y + this.h);
-  var maxy = Math.max(this.y, this.y + this.h);
-  return  (minx <= mx) && (maxx >= mx) &&
-          (miny <= my) && (maxy >= my);
-}
-
-Shape.prototype.border = function (mx, my) {
-  //Checks if within border
-  if (within_top(this, mx, my) && within_left(this, mx, my)) {
-    return Shape.prototype.borderenum.TOPLEFT;
-  } else if (within_top(this, mx , my) && within_right (this, mx, my)) {
-    return Shape.prototype.borderenum.TOPRIGHT;
-  } else if (within_bottom(this, mx, my) && within_right(this, mx, my)) {
-    return Shape.prototype.borderenum.BOTTOMRIGHT;
-  } else if (within_bottom(this, mx, my) && within_left(this, mx, my)) {
-    return Shape.prototype.borderenum.BOTTOMLEFT;
-  } else if (within_top(this, mx, my)) {
-    return Shape.prototype.borderenum.TOP;
-  } else if (within_left(this, mx, my)) {
-    return Shape.prototype.borderenum.LEFT;
-  } else if (within_right(this, mx, my)) {
-    return Shape.prototype.borderenum.RIGHT;
-  } else if (within_bottom(this, mx, my)) {
-    return Shape.prototype.borderenum.BOTTOM;
-  } else {
-    return false; 
-  }
-}
-
-Shape.prototype.borderenum = {
-  TOP : 1,
-  LEFT : 2,
-  RIGHT : 3,
-  BOTTOM : 4,
-  TOPLEFT : 5,
-  TOPRIGHT : 6,
-  BOTTOMLEFT : 7,
-  BOTTOMRIGHT : 8
-
-}
-
-function CanvasState(canvas) {
-  this.canvas = canvas;
-  this.width = canvas.width;
-  this.height = canvas.height;
-  this.ctx = canvas.getContext('2d');
-  // This complicates things a little but but fixes mouse co-ordinate problems
-  // when there's a border or padding. See getMouse for more detail
-  var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
-  if (document.defaultView && document.defaultView.getComputedStyle) {
-    this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
-    this.stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
-    this.styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
-    this.styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
-  }
-  // Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
-  // They will mess up mouse coordinates and this fixes that
-  var html = document.body.parentNode;
-  this.htmlTop = html.offsetTop;
-  this.htmlLeft = html.offsetLeft;
-
-  // **** Keep track of state! ****
-  
-  this.valid = false; // when set to false, the canvas will redraw everything
-  this.shapes = [];  // the collection of things to be drawn
-  this.dragging = false; // Keep track of when we are dragging
-  this.enlarging = false; // If enlarging the shape
-  // the current selected object. In the future we could turn this into an array for multiple selection
-  this.selection = null;
-  this.dragoffx = 0; // See mousedown and mousemove events for explanation
-  this.dragoffy = 0;
-  this.frame = 0;
-  
-  // **** Then events! ****
-  
-  // This is an example of a closure!
-  // Right here "this" means the CanvasState. But we are making events on the Canvas itself,
-  // and when the events are fired on the canvas the variable "this" is going to mean the canvas!
-  // Since we still want to use this particular CanvasState in the events we have to save a reference to it.
-  // This is our reference!
-  var myState = this;
-  
-  //fixes a problem where double clicking causes text to get selected on the canvas
-  canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
-  // Up, down, and move are for dragging
-  canvas.addEventListener('mousedown', function(e) {
-    var mouse = myState.getMouse(e);
-    var mx = mouse.x;
-    var my = mouse.y;
-    var shapes = myState.shapes;
-    var l = shapes.length;
-    for (var i = l-1; i >= 0; i--) {
-      var border = shapes[i].border(mx, my);
-      if (shapes[i].contains(mx, my)) {
-        var mySel = shapes[i];
-        // Keep track of where in the object we clicked
-        // so we can move it smoothly (see mousemove)
-        myState.dragoffx = mx - mySel.x;
-        myState.dragoffy = my - mySel.y;
-        myState.dragging = true;
-        myState.selection = mySel;
-        if (border) {
-          myState.enlarging = border;
-        }
-        myState.valid = false;
-        return;
-      } 
+class Shape {
+    constructor(x, y, w, h, fill) {
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+      this.fill = fill;
     }
-    // havent returned means we have failed to select anything.
-    // If there was an object selected, we deselect it
-    if (myState.selection) {
-      myState.selection = null;
-      myState.valid = false; // Need to clear the old selection border
+    within_top (shape, mx, my) {
+      return (shape.y + 4 >= my) && (shape.y - 4 <= my)
     }
-    var mouse = myState.getMouse(e);
-    var new_shape = new Shape(mouse.x, mouse.y, 1, 1, 'rgba(0,255,0,.6)');
-    myState.addShape(new_shape); 
-    myState.selection = new_shape;
-    myState.enlarging = Shape.prototype.borderenum.BOTTOMRIGHT;
-    
 
-  }, true);
-  canvas.addEventListener('mousemove', function(e) {
-    var mouse = myState.getMouse(e);
-    if (myState.enlarging == Shape.prototype.borderenum.RIGHT) {
-      myState.selection.w = mouse.x - myState.selection.x;  
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.LEFT) {
-      myState.selection.w += myState.selection.x - mouse.x; 
-      myState.selection.x = mouse.x; 
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.TOP) {
-      myState.selection.h += myState.selection.y - mouse.y; 
-      myState.selection.y = mouse.y; 
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.BOTTOM) {
-      myState.selection.h = mouse.y - myState.selection.y;  
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.BOTTOMRIGHT) {
-      myState.selection.w = mouse.x - myState.selection.x; 
-      myState.selection.h = mouse.y - myState.selection.y;  
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.BOTTOMLEFT) {
-      myState.selection.w += myState.selection.x - mouse.x; 
-      myState.selection.x = mouse.x;
-      myState.selection.h = mouse.y - myState.selection.y;   
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.TOPLEFT) {
-      myState.selection.w += myState.selection.x - mouse.x; 
-      myState.selection.x = mouse.x;
-      myState.selection.h += myState.selection.y - mouse.y; 
-      myState.selection.y = mouse.y;    
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.enlarging == Shape.prototype.borderenum.TOPRIGHT) {
-      myState.selection.h += myState.selection.y - mouse.y; 
-      myState.selection.y = mouse.y;    
-      myState.selection.w = mouse.x - myState.selection.x; 
-      myState.valid = false; // Something's dragging so we must redraw 
-    } else if (myState.dragging){
-      // We don't want to drag the object by its top-left corner, we want to drag it
-      // from where we clicked. Thats why we saved the offset and use it here
-      myState.selection.x = mouse.x - myState.dragoffx;
-      myState.selection.y = mouse.y - myState.dragoffy;   
-      myState.valid = false; // Something's dragging so we must redraw
+    within_bottom (shape, mx, my) {
+      return (shape.y + shape.h + 4 >= my) && (shape.y + shape.h - 4 <= my)
     }
-  }, true);
-  html.addEventListener("keypress", function(event) {
-    console.log(myState.selection);
-    if (event.keyCode == 32) {
-      if (myState.shapes.length > 0) {
-        myState.shapeNum = (myState.shapeNum + 1) % myState.shapes.length
-        myState.selection = myState.shapes[myState.shapeNum % myState.shapes.length];
-        console.log(myState.shapeNum);
-        myState.valid = false;
+
+    within_right (shape, mx, my) {
+      return (shape.x + shape.w + 4 >= mx) && (shape.x + shape.w - 4 <= mx);
+    }
+
+    within_left(shape, mx, my) {
+      return (shape.x + 4 >= mx) && (shape.x - 4 <= mx);
+    }
+    draw (ctx) {
+      ctx.fillStyle = this.fill;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    contains (mx, my) {
+      var minx = Math.min(this.x, this.x + this.w) - 2;
+      var maxx = Math.max(this.x, this.x + this.w) + 2;
+      var miny = Math.min(this.y, this.y + this.h) - 2;
+      var maxy = Math.max(this.y, this.y + this.h) + 2;
+      return  (minx <= mx) && (maxx >= mx) &&
+              (miny <= my) && (maxy >= my);
+    }
+
+    border(mx, my) {
+      //Checks if this.within border
+      if (this.within_top(this, mx, my) && this.within_left(this, mx, my)) {
+        return Shape.prototype.borderenum.TOPLEFT;
+      } else if (this.within_top(this, mx , my) && this.within_right (this, mx, my)) {
+        return Shape.prototype.borderenum.TOPRIGHT;
+      } else if (this.within_bottom(this, mx, my) && this.within_right(this, mx, my)) {
+        return Shape.prototype.borderenum.BOTTOMRIGHT;
+      } else if (this.within_bottom(this, mx, my) && this.within_left(this, mx, my)) {
+        return Shape.prototype.borderenum.BOTTOMLEFT;
+      } else if (this.within_top(this, mx, my)) {
+        return Shape.prototype.borderenum.TOP;
+      } else if (this.within_left(this, mx, my)) {
+        return Shape.prototype.borderenum.LEFT;
+      } else if (this.within_right(this, mx, my)) {
+        return Shape.prototype.borderenum.RIGHT;
+      } else if (this.within_bottom(this, mx, my)) {
+        return Shape.prototype.borderenum.BOTTOM;
+      } else {
+        return false; 
       }
     }
-  }, true);
-  html.addEventListener("keydown", function(event) {
-    if (myState.selection != null) {
-      if (event.keyCode == 38) {
-        myState.selection.y -= 1;
-      } else if (event.keyCode == 40) {
-        myState.selection.y += 1;
-      } else if (event.keyCode == 37) {
-        myState.selection.x -= 1;
-      } else if (event.keyCode == 39) {
-        myState.selection.x += 1;
-      } else if (event.keyCode == 68) {
-        myState.removeShape(myState.selection);
-        myState.selection = null;
-      }
-    } else if (event.keyCode == 37) {
-      if (myState.frame > 0) {
-        canvas.style.backgroundImage = frame_url(--(myState.frame));
-      }
-    } else if (event.keyCode == 39) {
-      canvas.style.backgroundImage = frame_url(++(myState.frame));
+
+    move_right(x_offset) {
+      this.w = x_offset - this.x;
     }
-    myState.valid = false;
 
-  }, true);
-  canvas.addEventListener('mouseup', function(e) {
-    myState.dragging = false;
-    myState.enlarging = false;
-    for (var i = 0; i < myState.shapes.length; i++) {
-      if (myState.shapes[i].w == 1 || myState.shapes[i].h == 1) {
-        myState.removeShape(myState.shapes[i]);
-        myState.selection = null;
-        myState.valid = false;
-      }
+    move_left(x_offset) {
+      this.w += this.x - x_offset; 
+      this.x = x_offset; 
     }
-  }, true);
-  canvas.addEventListener('dblclick', function(e) { e.preventDefault(); return false; },false);
-  
-  // **** Options! ****
-  myState.valid = false;
-  this.selectionColor = '#CC0000';
-  this.selectionWidth = 2;  
-  this.interval = 30;
-  setInterval(function() { myState.draw(); }, myState.interval);
-}
 
-CanvasState.prototype.addShape = function(shape) {
-  this.shapes.push(shape);
-  this.valid = false;
-}
-CanvasState.prototype.removeShape = function(shape) {
-  var index = this.shapes.indexOf(shape);
-  this.shapes.splice(index, 1); 
-  this.valid = false;
-}
-
-CanvasState.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
-}
-
-// While draw is called as often as the INTERVAL variable demands,
-// It only ever does something if the canvas gets invalidated by our code
-CanvasState.prototype.draw = function() {
-  // if our state is invalid, redraw and validate!
-  if (!this.valid) {
-    var ctx = this.ctx;
-    var shapes = this.shapes;
-    this.clear();
-    
-    // ** Add stuff you want drawn in the background all the time here **
-    
-    // draw all shapes
-    var l = shapes.length;
-    for (var i = 0; i < l; i++) {
-      var shape = shapes[i];
-      // We can skip the drawing of elements that have moved off the screen:
-      if (shape.x > this.width || shape.y > this.height ||
-          shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
-      shapes[i].draw(ctx);
+    move_top(y_offset) {
+      this.h += this.y - y_offset; 
+      this.y = y_offset; 
     }
-    
-    // draw selection
-    // right now this is just a stroke along the edge of the selected Shape
-    if (this.selection != null) {
-      ctx.strokeStyle = this.selectionColor;
-      ctx.lineWidth = this.selectionWidth;
-      var mySel = this.selection;
-      ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
 
-      /* Handles corners. */
-      ctx.fillStyle="#000000";
-      ctx.fillRect(mySel.x - 2, mySel.y - 2, 5, 5);
-      ctx.fillRect(mySel.x - 3 + mySel.w, mySel.y - 2, 5, 5);
-      ctx.fillRect(mySel.x - 2, mySel.y - 2 + mySel.h, 5, 5);
-      ctx.fillRect(mySel.x - 3 + mySel.w, mySel.y - 2 + mySel.h, 5, 5);
+    move_down(y_offset) {
+      this.h = y_offset - this.y; 
     }
-    
-    // ** Add stuff you want drawn on top all the time here **
-    
-    this.valid = true;
-  }
 }
+    Shape.prototype.borderenum = {
+      TOP : 1,
+      LEFT : 2,
+      RIGHT : 3,
+      BOTTOM : 4,
+      TOPLEFT : 5,
+      TOPRIGHT : 6,
+      BOTTOMLEFT : 7,
+      BOTTOMRIGHT : 8
 
-
-// Creates an object with x and y defined, set to the mouse position relative to the state's canvas
-// If you wanna be super-correct this can be tricky, we have to worry about padding and borders
-CanvasState.prototype.getMouse = function(e) {
-  var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
-  
-  // Compute the total offset
-  if (element.offsetParent !== undefined) {
-    do {
-      offsetX += element.offsetLeft;
-      offsetY += element.offsetTop;
-    } while ((element = element.offsetParent));
-  }
-
-  // Add padding and border style widths to offset
-  // Also add the <html> offsets in case there's a position:fixed bar
-  offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-  offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
-
-  mx = e.pageX - offsetX;
-  my = e.pageY - offsetY;
-  
-  // We return a simple javascript object (a hash) with x and y defined
-  return {x: mx, y: my};
-}
-
-video_name = "test_vid"
-total_frames = 99
-
-function frame_url(frame) {
-    block_0 = Math.floor(frame / 10000)
-    block_1 = Math.floor(frame / 100)
-    return `url('videos/${video_name}/${block_0}/${block_1}/${frame}.jpg')`
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  var s = new CanvasState(document.getElementById('frame'));
-});
+    }
 
