9c9a448ada1efd4e8e8d2c01a6da2150d4b3f8e1
Allen Wang <allen.y.w@berkeley.edu>
2016-06-02 16:50:15 -0700

    Updated padding

--- static/frame.js
+++ static/frame.js
@@ -7,3 +7,23 @@ function frame_url(frame) {
     return `url(/static/videos/${video_name}/${block_0}/${block_1}/${frame}.jpg)`
 }
 
+function frame_path(frame) {
+    block_0 = Math.floor(frame / 10000)
+    block_1 = Math.floor(frame / 100)
+    return `videos/${video_name}/${block_0}/${block_1}/${frame}.jpg`
+}
+
+function frame_width() {
+    return img.naturalWidth;
+}
+
+function frame_height() {
+    return img.naturalHeight;
+}
+
+var img = new Image();
+img.onload = function() {
+    alert(this.width + 'x' + this.height);
+}
+img.src = frame_path(1);
+console.log(frame_width());
