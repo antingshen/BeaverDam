d375a9c125545e5b2ebb8d3a9e5f4048a0b3b10a
Allen Wang <allen.y.w@berkeley.edu>
2016-06-08 23:24:52 -0700

    Created horizontal scroll bar

--- static/frame.js
+++ static/frame.js
@@ -10,12 +10,39 @@ function frame_path(frame) {
 function frame_url(frame) {
     return `url(` + frame_path(frame) + `)`;
 }
-function preload(endFrame) {
+function frame_preload(endFrame) {
     for (var i = 0; i < endFrame; i++) {
         images[i] = new Image();
         images[i].src = frame_path(i);
     }
+    return images[0];
 }
+function update_frame(state, val) {
+    document.getElementById("range").innerHTML=val;
+    state.frame = val;
+    document.getElementById("frame").style.backgroundImage = frame_url(state.frame);
+    state.valid = false;
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var scroll = false;
+    var img = frame_preload(500);
+    img.onload = function () {
+        var canvas = new Canvas(document.getElementById('frame'));
+        var scrollBar = document.getElementById("scroll-bar");
+        scrollBar.addEventListener("mouseup", function(){
+            scroll = false;
+        });
+        scrollBar.addEventListener("click", function(){
+            scroll = true;
+            update_frame(canvas, scrollBar.value);
+        });
+        scrollBar.addEventListener("mousemove", function() {
+            if (scroll) {
+                update_frame(canvas, scrollBar.value);
+            }
+        });
+    };
 
-preload(500);
+
+});
 
