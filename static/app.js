d69d2da305e9564c202930da487cef22a9a943aa
Allen Wang <allen.y.w@berkeley.edu>
2016-06-08 23:24:52 -0700

    Created horizontal scroll bar

--- static/app.js
+++
@@ -1,9 +0,0 @@
-var LABELS_LIST = ["Car", "Truck", "Tree", "Human"];
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  var img = new Image();
-  img.src = frame_path(0);
-  img.onload = function () {
-    new Canvas(document.getElementById('frame'));
-  }
-});
