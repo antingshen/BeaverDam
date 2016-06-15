15b932278ddb1cbbca543391006aefe8e6004b6e
Anting Shen <shenantingtt@hotmail.com>
2016-06-15 16:26:32 -0700

    Cleaned up some js/css, added minor css enhancements

--- static/frame.js
+++ static/frame.js
@@ -10,7 +10,7 @@ function frame_path(frame) {
 }
 
 function frame_url(frame) {
-    return `url(` + frame_path(frame) + `)`;
+    return `url(${frame_path(frame)}`;
 }
 
 function frame_preload(endFrame) {
@@ -23,39 +23,30 @@ function frame_preload(endFrame) {
     return images[0];
 }
 
-function update_frame(state, val) {
-    frame_preload(val);
-    var num = document.getElementById("frame-number");
-    document.getElementById("scroll-bar").value = val;
-    num.setAttribute("value", val);
-    state.frame = parseInt(val);
-    document.getElementById("frame").style.backgroundImage = frame_url(state.frame);
-    state.valid = false;
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     var img = frame_preload(PREFETCH_NUMBER);
     img.onload = function () {
-        var canvas = new Canvas(document.getElementById('frame'));
+        canvas = new Canvas(document.getElementById('frame'));
         var scrollBar = document.getElementById("scroll-bar");
         var playButton = document.getElementById("play-button");
         scrollBar.addEventListener("click", function(){
-            update_frame(canvas, scrollBar.value);
+            canvas.frame = scrollBar.value;
         });
 
         scrollBar.addEventListener("input", function() {
-            update_frame(canvas, scrollBar.value);
+            canvas.frame = scrollBar.value;
         });
         document.addEventListener("keypress", function(event) {
             if (event.keyCode === 13) { //If Enter is pressed
                 scrollBar.value = document.getElementById("frame-number").value;
-                update_frame(canvas, scrollBar.value);
+                canvas.frame = scrollBar.value;
             }
         });
         playButton.addEventListener("click", function(){
             canvas.play = !canvas.play;
+            playButton.className = `glyphicon glyphicon-${canvas.play ? 'pause' : 'play'}`;
         })
-        update_frame(canvas, 0);
+        canvas.frame = 0;
     };
 
 });
