c5d111c078314b4e0824d4f878d028d46b3480fa
Sean Zhu <sean.zhu@berkeley.edu>
2016-07-24 05:01:17 -0700

    Clean up imports

--- beaverdam/urls.py
+++ beaverdam/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from django.views.generic.base import RedirectView
 
-from annotator.views import *
+from annotator.views import AnnotationView
+from annotator.views import home, verify, video
+from django.contrib.auth.views import login, logout
+from django.views.generic.base import RedirectView
 
 admin.site.site_header = 'BeaverDam'
 
@@ -12,11 +14,11 @@ urlpatterns = [
     url(r'^video/(\d+)/verify/$', verify),
     url(r'^annotation/(\d+)', AnnotationView.as_view()),
 
-    url(r'^login/$', 'django.contrib.auth.views.login', 
+    url(r'^login/$', login, 
         {'template_name': 'admin/login.html', 
             'extra_context': {'site_header': 'BeaverDam Login'}
         }),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^logout/$', logout),
     url(r'^accounts/', RedirectView.as_view(url='/')),
     url(r'^admin/', admin.site.urls),
 ]
